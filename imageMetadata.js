2b90fd7f239e2976f6c7f22c0b3a8206fef8c37c
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-13 13:13:25 -0700

    Got a full working manifest creator/updater

--- imageMetadata.js
+++ imageMetadata.js
@@ -1,30 +1,69 @@
 const AWS = require('aws-sdk');
 
-const s3 = new AWS.S3(); // This object provides an interface for S3
+const s3 = new AWS.S3();
 
 exports.handler = async (event) => {
-    
-    console.log(event.Records);
-    
-    let imageKey = event.Records[0].s3.bucket.object.key;
-    let imageSize = event.Records[0].s3.bucket.object.size;
-    let imageLastUpdated = event.Records[0].eventTime;
-    let manifestRecord = {
-      imageKey,
-      imageSize,
-      imageLastUpdated,
-    }
+  let payload = {
+    statusCode: 500,
+    body: 'Lambda function encountered an unknown error',
+  };
 
-    let bucketName = event.Records[0].s3.bucket.name;
-    
-    let object = await s3.getObject({ Bucket: bucketName, Key: 'images.json' }).promise();
-    let json = JSON.parse(object.Body.toString());
-    
-    console.log('Logging S3 json', json)
-    
-    const payload = {
-        statusCode: 200,
-        body: JSON.stringify('Hello world'), // Stringify any objects sent in the body
+  const bucketName = event.Records[0].s3.bucket.name;
+  const manifestKey = 'images.json';
+
+  let oldManifest = {};
+
+  try {
+    const rawManifest = await s3.getObject({ Bucket: bucketName, Key: manifestKey }).promise();
+    oldManifest = JSON.parse(rawManifest.Body.toString());
+    console.log('\nOld manifest:\n ', oldManifest);
+  } catch (error) {
+    console.error('Existing manifest not found\n', error);
+  }
+
+  const imageKey = event.Records[0].s3.object.key;
+  const imageSize = event.Records[0].s3.object.size;
+  const imageLastUpdated = event.Records[0].eventTime;
+  let record = {};
+  record[imageKey] = {
+    imageKey,
+    imageSize,
+    imageLastUpdated,
+  };
+
+  if (oldManifest[imageKey]) {
+    console.log(
+      `\nRecord found in manifest for ${imageKey}, overwriting with new data:\n `,
+      record[imageKey],
+    );
+  }
+
+  const newManifest = {
+    ...oldManifest,
+    ...record,
+  };
+
+  console.log('\nNew manifest:\n ', newManifest);
+
+  try {
+    await s3
+      .putObject({
+        Bucket: bucketName,
+        Key: manifestKey,
+        Body: JSON.stringify(newManifest),
+      })
+      .promise();
+
+    payload = {
+      statusCode: 200,
+      body: `File uploaded successfully at http://${bucketName}.s3.amazonaws.com/${manifestKey}`,
     };
-    return payload;
+  } catch (error) {
+    console.error(error);
+    payload = {
+      statusCode: error.statusCode || 404,
+      body: JSON.stringify(error),
+    };
+  }
+  return payload;
 };
