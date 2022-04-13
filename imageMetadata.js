648eaf69b08a38716f374ae05d836cc56e9a3ecf
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-13 08:53:06 -0700

    Created partial lambda function

---
+++ imageMetadata.js
@@ -0,0 +1,31 @@
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3(); // This object provides an interface for S3
+
+exports.handler = async (event) => {
+    
+    console.log(event.Records);
+    
+    let bucketName = event.Records[0].s3.bucket.name;
+    let imageKey = event.Records[0].s3.bucket.object.key;
+    let imageSize = event.Records[0].s3.bucket.object.size;
+    let imageLastUpdated = event.Records[0].eventTime;
+    let manifestRecord = {
+      imageKey,
+      imageSize,
+      imageLastUpdated,
+    }
+    let key = '';
+    
+    // let object = await s3.getObject({ Bucket: bucketName, Key: key }).promise();
+
+    // let json = JSON.parse(object.Body.toString());
+    
+    // console.log('Logging S3 json', json)
+    
+    const payload = {
+        statusCode: 200,
+        body: JSON.stringify('Hello world'), // Stringify any objects sent in the body
+    };
+    return payload;
+};
